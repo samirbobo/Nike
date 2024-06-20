4adda3caddeaf3f9b1bedfe63f7fdf27104d7877
samir <samir.elanany@microengineering.ca>
2024-06-20 20:39:25 +0300

    finished build task

--- src/Sections/Services.jsx
+++ src/Sections/Services.jsx
@@ -1,3 +1,29 @@
+import { services } from "../constants/index";
+
 export default function Services() {
-  return <section className="padding-x py-10">Services</section>;
+  return (
+    <section className="max-container padding-x py-10 grid gap-9 lg:grid-cols-3 md:grid-cols-2">
+      {services.map((service) => (
+        <div
+          key={service.label}
+          className="px-10 py-16 rounded-[20px] shadow-3xl bg-white"
+        >
+          <div className="w-11 h-11 flex justify-center items-center bg-coral-red rounded-full">
+            <img
+              src={service.imgURL}
+              alt="service image"
+              width={24}
+              height={24}
+            />
+          </div>
+          <h2 className="font-palanquin font-bold leading-normal text-3xl mt-5">
+            {service.label}
+          </h2>
+          <p className="text-slate-gray font-montserrat leading-normal break-words text-lg mt-3">
+            {service.subtext}
+          </p>
+        </div>
+      ))}
+    </section>
+  );
 }
--- src/Sections/SpecialOffer.jsx
+++ src/Sections/SpecialOffer.jsx
@@ -1,3 +1,43 @@
+import Button from "../Components/Button";
+import { arrowRight } from "../assets/icons";
+import { offer } from "../assets/images";
+
 export default function SpecialOffer() {
-  return <section className="padding">Special Offer</section>;
+  return (
+    <section className="padding flex justify-center items-center max-lg:flex-col-reverse gap-10 max-container">
+      <div className="flex flex-1">
+        <img
+          src={offer}
+          alt="offer"
+          width={773}
+          height={687}
+          className="object-contain w-full"
+        />
+      </div>
+      <article className="flex-1">
+        <h2 className="font-palanquin capitalize font-bold text-4xl">
+          <span className="text-coral-red"> Special </span> Offer
+        </h2>
+        <p className="mt-4 info-text">
+          Embark on a shopping journey that redefines your experience with
+          unbeatable deals. From premier selections to incredible savings, we
+          offer unparalleled value that sets us apart.
+        </p>
+        <p className="mt-6 info-text">
+          Navigate a realm of possibilities designed to fulfill your unique
+          desires, surpassing the loftiest expectations. Your journey with us is
+          nothing short of exceptional.
+        </p>
+        <div className="flex flex-wrap gap-4 mt-12">
+          <Button label="Shop Now" iconURL={arrowRight} />
+          <Button
+            label="Learn more"
+            backgroundColor="bg-white"
+            borderColor="border-slate-gray"
+            textColor="text-slate-gray"
+          />
+        </div>
+      </article>
+    </section>
+  );
 }
